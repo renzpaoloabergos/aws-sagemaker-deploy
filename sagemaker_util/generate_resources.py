9e8ed1850c9c83e73800cbe74e5aa46e66f9a0a3
Renz Abergos <renz@kumu.ph>
2022-02-17 11:11:02 +0800

    Add another subnet in different availability zone

--- sagemaker_util/generate_resources.py
+++ sagemaker_util/generate_resources.py
@@ -20,7 +20,7 @@ def gen_model(model_name, image_tag, execution_role, api_name, timeout, num_of_w
                 },
                 "VpcConfig": { 
                     "SecurityGroupIds": [ "sg-0261032fe0e4fe381" ],
-                    "Subnets": [ "subnet-0baf1f627590e5b0b" ]
+                    "Subnets": [ "subnet-0baf1f627590e5b0b", "subnet-080e06bbcf2cea60b" ]
                 }
             },
         },
