aabcda6b85ba8d91045eb769c44026afcae482e3
Renz Abergos <renz@kumu.ph>
2022-04-25 15:06:07 +0800

    Add new vpc subnets

--- sagemaker_util/generate_resources.py
+++ sagemaker_util/generate_resources.py
@@ -18,9 +18,14 @@ def gen_model(model_name, image_tag, execution_role, api_name, timeout, num_of_w
                         "BENTOML_GUNICORN_NUM_OF_WORKERS": num_of_workers,
                     },
                 },
-                "VpcConfig": { 
-                    "SecurityGroupIds": [ "sg-0261032fe0e4fe381" ],
-                    "Subnets": [ "subnet-0baf1f627590e5b0b", "subnet-080e06bbcf2cea60b" ]
+                "VpcConfig": {
+                    "SecurityGroupIds": ["sg-0261032fe0e4fe381"],
+                    "Subnets": [
+                        "subnet-0baf1f627590e5b0b",
+                        "subnet-080e06bbcf2cea60b",
+                        "subnet-0d68809d11ca254ba",
+                        "subnet-0795c0953df76238c"
+                    ]
                 }
             },
         },
